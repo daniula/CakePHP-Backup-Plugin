4c4406ec2a325a072d4e81729e80375a4e5afce6
Daniel Mendalka <daniel.mendalka@gmail.com>
2013-02-01 22:28:56 +0100

    init commit with basic funcionality -> generating mysqldumos

---
+++ Console/Command/DbShell.php
@@ -0,0 +1,64 @@
+<?php
+
+App::uses('ConnectionManager', 'Model');
+
+define('DUMP_PATH', dirname(dirname(dirname(__FILE__))).DS.'tmp'.DS );
+
+class DbShell extends AppShell {
+  private $changes = array();
+
+  public function main() {
+    $db = ConnectionManager::getDataSource('default');
+
+    $login = ConnectionManager::$config->default['login'];
+    $password = ConnectionManager::$config->default['password'];
+    $database = ConnectionManager::$config->default['database'];
+    $options = '--skip-opt --no-create-info';
+
+    $tables = $db->listSources();
+    foreach ($tables as $table) {
+      $modelName = Inflector::singularize(ucfirst($table));
+      $this->loadModel($modelName);
+
+      if ($this->hasChanges( $this->{$modelName} )) {
+        $cmd = sprintf('mysqldump -u %s -p%s %s %s %s > %s.sql', $login, $password, $options, $database, $table, DUMP_PATH.$table);
+        $this->out($cmd);
+        exec($cmd);
+      }
+    }
+  }
+
+  private function hasChanges($model) {
+    $schema = $model->schema();
+
+    if (!empty($schema['updated'])) {
+      $fields = array('MAX(updated) AS lastUpdate');
+      $lastUpdate = Set::extract($model->find('first', compact('fields')), '0.lastUpdate');
+      $result = $this->checkChanges($lastUpdate, $model->alias);
+    } else {
+      $count = $model->find('count');
+      $result = $this->checkChanges($count, $model->alias);
+    }
+
+    return $result;
+  }
+
+  private function checkChanges($value, $modelName) {
+    $result = false;
+
+    if ( empty($this->changes) && ($cache = Cache::read('Backup.Db'))) {
+      $this->changes = $cache;
+    }
+
+    if (empty($this->changes[$modelName]) || $this->changes[$modelName] < $value) {
+      $this->changes[$modelName] = $value;
+      $result = true;
+    }
+
+    return $result;
+  }
+
+  private function saveChanges() {
+    Cache::write('Backup.Db', $this->changes);
+  }
+}
---
+++ tmp/logs.sql
@@ -0,0 +1,46 @@
+-- MySQL dump 10.13  Distrib 5.5.19, for osx10.7 (i386)
+--
+-- Host: localhost    Database: sellbox
+-- ------------------------------------------------------
+-- Server version	5.5.9
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `logs`
+--
+
+INSERT INTO `logs` VALUES (4,'fb_source','31');
+INSERT INTO `logs` VALUES (9,'m2','15');
+INSERT INTO `logs` VALUES (10,'m3','13');
+INSERT INTO `logs` VALUES (11,'m1','32');
+INSERT INTO `logs` VALUES (12,'m4','11');
+INSERT INTO `logs` VALUES (13,'dmq','2');
+INSERT INTO `logs` VALUES (14,'all','12');
+INSERT INTO `logs` VALUES (15,'m5','10');
+INSERT INTO `logs` VALUES (17,'m6','18');
+INSERT INTO `logs` VALUES (18,'m7','9');
+INSERT INTO `logs` VALUES (19,'m8','21');
+INSERT INTO `logs` VALUES (20,'qr','3');
+INSERT INTO `logs` VALUES (21,'db','2');
+INSERT INTO `logs` VALUES (22,'dm','3');
+INSERT INTO `logs` VALUES (23,'fbm1','1');
+INSERT INTO `logs` VALUES (24,'email','81');
+INSERT INTO `logs` VALUES (25,'utm_medium','9');
+INSERT INTO `logs` VALUES (26,'utm_source','9');
+INSERT INTO `logs` VALUES (27,'fbm4','2');
+INSERT INTO `logs` VALUES (28,'p4l','8');
+INSERT INTO `logs` VALUES (29,'gplus','1');
+INSERT INTO `logs` VALUES (30,'ref','2');
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-02-01 22:27:57
